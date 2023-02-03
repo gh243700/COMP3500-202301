a894ca3fc16f35a876f46bbec56d4087da125462
JEL97 <gh243700@gmail.com>
2023-02-03 19:06:14 +0900

    Update Indent.java

--- Assignment2/src/academy/pocu/comp3500/assignment2/Indent.java
+++ Assignment2/src/academy/pocu/comp3500/assignment2/Indent.java
@@ -12,7 +12,7 @@ public final class Indent {
     public Indent() {
     }
 
-    public Indent(String message, Indent parent) {
+    private Indent(String message, Indent parent) {
         this.message = message;
         this.parent = parent;
     }
