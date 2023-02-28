2b3d7b4e2df697bda95d9b453e5e46bea8bc0047
JEL97 <gh243700@gmail.com>
2023-02-28 16:02:02 +0900

    Update Program.java

--- Lab7/src/academy/pocu/comp3500/lab7/app/Program.java
+++ Lab7/src/academy/pocu/comp3500/lab7/app/Program.java
@@ -1,8 +1,53 @@
 package academy.pocu.comp3500.lab7.app;
 
+import academy.pocu.comp3500.lab7.Decryptor;
+
 public class Program {
 
     public static void main(String[] args) {
 	    // write your code here
+        {
+
+            {
+            String[] codeWords = new String[]{"cat", "CATS", "AcTS", "SCAN", "acre", "aNTS"};
+
+            Decryptor decryptor = new Decryptor(codeWords);
+
+            String[] candidates = decryptor.findCandidates("cat");
+
+            assert (candidates.length == 1);
+            assert (candidates[0].equals("cat"));
+
+            candidates = decryptor.findCandidates("race");
+
+            assert (candidates.length == 1);
+            assert (candidates[0].equals("acre"));
+
+            candidates = decryptor.findCandidates("ca");
+
+            assert (candidates.length == 0);
+
+            candidates = decryptor.findCandidates("span");
+
+            assert (candidates.length == 0);
+
+            candidates = decryptor.findCandidates("ACT");
+
+            assert (candidates.length == 1);
+            assert (candidates[0].equals("cat"));
+
+            candidates = decryptor.findCandidates("cats");
+
+            assert (candidates.length == 2);
+            assert (candidates[0].equals("cats") || candidates[0].equals("acts"));
+            assert (candidates[1].equals("cats") || candidates[1].equals("acts"));
+
+            candidates = decryptor.findCandidates("SCAt");
+
+            assert (candidates.length == 2);
+            assert (candidates[0].equals("cats") || candidates[0].equals("acts"));
+            assert (candidates[1].equals("cats") || candidates[1].equals("acts"));
+        }
+        }
     }
 }
