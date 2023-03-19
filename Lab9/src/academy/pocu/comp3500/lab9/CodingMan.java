cce86cce3d60278a6e72afaeba3f11d818aba021
JEL97 <gh243700@gmail.com>
2023-03-19 16:54:33 +0900

    Update CodingMan.java

--- Lab9/src/academy/pocu/comp3500/lab9/CodingMan.java
+++ Lab9/src/academy/pocu/comp3500/lab9/CodingMan.java
@@ -2,6 +2,8 @@ package academy.pocu.comp3500.lab9;
 
 import academy.pocu.comp3500.lab9.data.VideoClip;
 
+import java.util.Arrays;
+
 public class CodingMan {
     public static int findMinClipsCount(final VideoClip[] clips, int time) {
         sort(clips);
@@ -65,20 +67,6 @@ public class CodingMan {
     }
 
     public static void sort(final VideoClip[] clips) {
-        for (int i = 0; i < clips.length; ++i) {
-            for (int k = 0; k < clips.length - i - 1; ++k) {
-                if (clips[k].getStartTime() > clips[k + 1].getStartTime()) {
-                    VideoClip temp = clips[k];
-                    clips[k] = clips[k + 1];
-                    clips[k + 1] = temp;
-                } else if (clips[k].getStartTime() == clips[k + 1].getStartTime()) {
-                    if (clips[k].getEndTime() < clips[k + 1].getEndTime()) {
-                        VideoClip temp = clips[k];
-                        clips[k] = clips[k + 1];
-                        clips[k + 1] = temp;
-                    }
-                }
-            }
-        }
+        Arrays.sort(clips, ((a, b) -> a.getStartTime() - b.getStartTime()));
     }
 }
